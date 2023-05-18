4999485a69443ced805eb99cf1009c3c6a007018
alex8023 <alex8023@yandex.ru>
2023-05-18 17:31:38 +0300

    add general elements

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,63 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import styled, { css } from 'styled-components';
+
+const Header = styled.header`
+  outline: 1px solid coral;
+`;
+
+const Table = styled.table`
+  border: 1px solid grey;
+  border-collapse: collapse;
+`;
+
+const Tbody = styled.tbody`
+  border: 1px solid grey;
+`;
+
+const Th = styled.th`
+  position: relative;
+  top: -10px;
+`;
+
+const Td = styled.td`
+  border: 1px solid grey;
+`;
 
 function App() {
+  // const [isActive, setIsActive] = useState('isActive');
+  const today = new Date();
+  const monday = new Date(
+    today.setDate(today.getDate() - today.getDay() + 1)
+  ).toUTCString();
+
+  const hours = new Array(24).fill('hour').map((hour, index) => {
+    if (!index) {
+      return;
+    } else if (index < 10) {
+      return `0${index}:00`;
+    } else return `${index}:00`;
+  });
+  const days = new Array(7).fill('day');
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <Header>Interview Calendar</Header>
+      <div>дни недели, дата: {monday} и месяц</div>
+      <Table>
+        <Tbody>
+          {hours.map((hour) => (
+            <tr>
+              <Th>{hour}</Th>
+              {days.map((day) => (
+                <Td>day</Td>
+              ))}
+            </tr>
+          ))}
+        </Tbody>
+      </Table>
+
+      <footer>Today (Delete)</footer>
     </div>
   );
 }
