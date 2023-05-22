83c52b6a035361326c661b01f565c2738426b561
alex8023 <alex8023@yandex.ru>
2023-05-22 15:58:14 +0300

    add calMatrix & isTakenArr

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,9 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import styled, { css } from 'styled-components';
 import { previousMonday, isMonday } from 'date-fns';
+import { th } from 'date-fns/locale';
 
 const Header = styled.header`
   outline: 1px solid coral;
@@ -22,50 +23,93 @@ const Th = styled.th`
   top: -10px;
 `;
 
-const Td = styled.td`
+const Td = styled.td<{ isTaken: boolean }>`
   border: 1px solid grey;
+  ${({ isTaken }) =>
+    isTaken &&
+    `
+    background: blue;
+  `}
 `;
 
+interface HourObj {
+  hourStr: string;
+  hourDaysArr: { dateTime: number; isTaken: boolean }[];
+}
+
 function App() {
-  // const [isActive, setIsActive] = useState('isActive');
-  const today = new Date();
-  const monday = isMonday(today)
-    ? new Date(new Date().setHours(0, 0, 0))
-    : new Date(previousMonday(today).setHours(0, 0, 0));
+  const [calMatrix, setCalMatrix] = useState<HourObj[]>([]);
+  const [isTakenArr, setIsTakenArr] = useState<number[]>([]);
 
-  const hours = new Array(24).fill('').map((hour, index) => {
-    if (!index) {
-      return;
-    } else if (index < 10) {
-      return `0${index}:00`;
-    } else return `${index}:00`;
-  });
+  useEffect(() => {
+    const today = new Date();
+    const monday = isMonday(today)
+      ? new Date(new Date().setHours(0, 0, 0, 0))
+      : new Date(previousMonday(today).setHours(0, 0, 0, 0));
 
-  const days = new Array(7).fill('day');
+    const days = new Array(7).fill('').map((day, index) => {
+      const nextDay = new Date(monday);
+      nextDay.setDate(monday.getDate() + index);
+      return nextDay;
+    });
+
+    const hours = new Array(24).fill('').map((hour, index) => {
+      let hourStr;
+      if (!index) {
+        hourStr = '';
+      } else if (index < 10) {
+        hourStr = `0${index}:00`;
+      } else {
+        hourStr = `${index}:00`;
+      }
+      const hourDaysArr = days.map((day) => {
+        const dateTime = day.setHours(index);
+        // const dateTime = new Date(day.setHours(index));
+        let isTaken;
+        if (isTakenArr.includes(dateTime)) {
+          isTaken = true;
+        } else {
+          isTaken = false;
+        }
+        return { dateTime, isTaken };
+      });
+
+      return { hourStr, hourDaysArr };
+    });
+
+    console.log(days, hours);
+
+    setCalMatrix(hours);
+  }, [isTakenArr]);
 
   function addTask() {
-    const taskDate =
+    const taskDateStr =
       prompt(
         `
-    Enter event time: 
+    Enter event time:  
     YYYY-MM-DD HH:MM:ss`,
-        '2023-05-15 05:15:22'
+        '2023-05-25 05:15:22'
       ) ?? '';
 
+    if (taskDateStr) {
+      // const taskDateTStamp = new Date(taskDateStr).setMinutes(0, 0, 0);
+      setIsTakenArr((st) => [...st, new Date(taskDateStr).setMinutes(0, 0, 0)]);
+    }
+
     console.log(
-      taskDate,
+      taskDateStr,
       '&',
-      new Date(taskDate).setMinutes(0, 0, 0),
+      new Date(taskDateStr).setMinutes(0, 0, 0),
       '&',
-      new Date(new Date(taskDate).setMinutes(0, 0, 0)),
+      new Date(new Date(taskDateStr).setMinutes(0, 0, 0)),
       '&',
-      new Date(new Date(taskDate).setMinutes(0, 0, 0)).toISOString(),
+      new Date(new Date(taskDateStr).setMinutes(0, 0, 0)).toISOString(),
       '&',
       new Date(new Date().setMinutes(0, 0, 0)).toISOString(),
       '&',
-      previousMonday(new Date(taskDate)),
+      previousMonday(new Date(taskDateStr)),
       '&',
-      new Date(previousMonday(new Date(taskDate)).setHours(0, 0, 0))
+      new Date(previousMonday(new Date(taskDateStr)).setHours(0, 0, 0))
     );
   }
 
@@ -74,14 +118,23 @@ function App() {
       <Header>
         Interview Calendar <button onClick={addTask}>+</button>
       </Header>
-      <div>дни недели, дата: {monday.toISOString()} и месяц</div>
+      {/* <div>дни недели, дата: {monday.toISOString()} и месяц</div> */}
       <Table>
+        <thead>
+          <tr>
+            <th></th>
+            {calMatrix[0]?.hourDaysArr.map((day) => (
+              <th>{new Date(day.dateTime).getDate()}</th>
+            ))}
+          </tr>
+        </thead>
         <Tbody>
-          {hours.map((hour) => (
+          {calMatrix.map((hour) => (
             <tr>
-              <Th>{hour}</Th>
-              {days.map((day) => (
-                <Td>day</Td>
+              <Th>{hour.hourStr}</Th>
+              {hour.hourDaysArr.map((day) => (
+                // <Td isTaken={day.isTaken}>{new Date(day.dateTime).toString()}</Td>
+                <Td isTaken={day.isTaken}>{day.dateTime}</Td>
               ))}
             </tr>
           ))}
